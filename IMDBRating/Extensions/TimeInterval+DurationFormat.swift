0dea92e883fadd810eeb68d9605ee28b20231217
Nikolay <infnick@list.ru>
2025-10-24 11:15:32 +0500

    Add extension for time interval and grouped other extensions

---
+++ IMDBRating/Extensions/TimeInterval+DurationFormat.swift
@@ -0,0 +1,20 @@
+//
+//  Untitled.swift
+//  IMDBRating
+//
+//  Created by Nikolay - on 23.10.2025.
+//
+import Foundation
+
+extension TimeInterval {
+    func formatDuration(from timeInterval: TimeInterval) -> String? {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute] // Specify the units to display
+        formatter.unitsStyle = .abbreviated // Options include .abbreviated, .short, and .full
+        
+        // Set padding for single-digit units (e.g., "05h 02m")
+        formatter.zeroFormattingBehavior = .pad
+        
+        return formatter.string(from: timeInterval)
+    }
+}
